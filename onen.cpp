7b6945de32457e130c6237e4b6a0aa2f84ca655f
Hanzala <bigapplestorekmr@gmail.com>
2025-05-22 19:15:05 +0530

    another

---
+++ onen.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main () {
+    int i,j,count=5;
+    for(i=1; i<=5; i++) {
+
+        for (j=1; j<=5; j++ ){
+            if(j>=count){
+                cout<<i;
+            }else {
+                cout << " ";
+            }
+        }
+        cout<<endl;
+        count = count-1;
+    }
+}
